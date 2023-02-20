c4119f4c57e677700ac244b36e8663eacae35287
Four Source Technologies <122277569+4SourceTechnologies@users.noreply.github.com>
2023-02-20 16:14:08 +0530

    added updated files

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-$('.mainNavToggler').click(function(){
-	$('.mobileNav').toggleClass('show')
-})
+
+// Mobile nav toggle button
+$(".mainNavToggler").click(function () {
+  $(".mobileNav").toggleClass("show");
+});
+
+// Animated circle Intersection obserrver
+
+var circle = document.querySelector(".animatedCircle");
+var imgBoxes = document.querySelectorAll(".imgBx");
+
+const circleOptions = {
+  threshold: "0.9",
+};
+
+const circleObserver = new IntersectionObserver(function (
+  entries,
+  circleObserver
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      imgBoxes.forEach((imgBox) => imgBox.classList.add("animateThree"));
+    }
+  });
+},
+circleOptions);
+
+circleObserver.observe(circle);
